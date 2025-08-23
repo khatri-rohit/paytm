3141dd4774afcddc984ef451644a99d2b9d95e80
Rohit Khatri <rohitkhatri111112@gmail.com>
2025-08-23 20:24:25 +0530

    -- Add prisma into turborepo

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,18 +1,22 @@
-"use client";
 
-import { RootState, useAppSelector } from '@repo/store';
+import { prisma } from '@repo/db';
+// import { RootState, useAppSelector } from '@repo/store';
 import { TurborepoLogo } from '@repo/ui/turborepo-logo';
 
-const Page = () => {
-  const user = useAppSelector((state: RootState) => {
-    return state;
-  });
+const Page = async () => {
+  // const user = useAppSelector((state: RootState) => {
+  //   return state;
+  // });
+
+  const User = await prisma.user.findFirst();
 
   return (
     <main className='min-h-screen p-24'>
       <TurborepoLogo />
       <p className="text-3xl">
-        PayTM User {user.name}
+        PayTM User {User?.name ?? "No user added yet"}
+
+        {/* {User.then((data) => data.name) || "No User"} */}
       </p>
     </main>
   );
